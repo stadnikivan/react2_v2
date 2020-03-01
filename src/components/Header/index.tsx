9a901dd3642389e5641161ab07b7e7a61c2c1ae9
stadnikivan <39565192+stadnikivan@users.noreply.github.com>
2020-03-01 22:16:23 +0200

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import style from './style.module.css'
-import Menu from "./Menu"
 
 
 const Header:React.FC = () => {
     return <div className={style.ButtonsBlock}>
-        <Menu/>
      </div>
   }
 
